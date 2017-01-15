d02e074131b845647b73da42408070f4bcbc5637
tzl <torontozinelibrary@gmail.com>
2017-01-15 13:41:28 -0500

    first

---
+++ run.js
@@ -0,0 +1,47 @@
+var canvas;
+var runners;
+
+function Runner(pos){
+    this.y = pos;
+    this.x = 0;
+    this.speed = Math.random();
+    this.fail = Math.random() * 2 / 1000;
+    this.failed = false;
+    this.red = Math.floor(Math.random() * 255);
+    this.blue = Math.floor(Math.random() * 255);
+    this.green = Math.floor(Math.random() * 255);
+}
+
+Runner.prototype.draw = function(){
+    fill(this.red, this.blue, this.green);
+    rect(this.x, this.y, 40, 40);
+}
+
+Runner.prototype.update = function(){
+    if (! this.failed){
+        this.x += this.speed;
+    }
+    if (Math.random() < this.fail){
+        this.failed = true;
+    }
+}
+
+Runner.prototype.reset = function(){
+    this.x = 0;
+}
+
+function setup(){
+    canvas = createCanvas(800, 800);
+    runners = [];
+    for (var i = 0; i < 10; i++){
+        runners.push(new Runner(i * 40 + 5));
+    }
+}
+
+function draw(){
+    clear();
+    Array.forEach(runners, function(runner){
+        runner.draw();
+        runner.update();
+    });
+}
